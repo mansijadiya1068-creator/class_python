0117e06214f0d8e86e69fb90c14c4249c8607db1
mansijadiya1068-creator <mansijadiya1068@gmail.com>
2026-01-27 08:14:55 +0530

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,36 @@
+#arithmetic operater
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a/b)
+#assignment operater
+#assignment value 10 to x
+x=22
+print(x)
+#unary minus
+a=33
+print(-a)
+#relational operater
+p=55
+q=77
+print(p>q)
+print(p==q)
+#logical operater.py
+x=True
+y=False
+print (x and y)
+print(x or y)
+print(not x)
+#boolean
+is_pass
+print(is_pass)
+#bitwise 
+a=3
+b=9
+print(a&b)
+print(a/b)
+#membership
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)
