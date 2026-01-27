c039fe454460d3bc0021a6dcbaedb4d72eb4aee9
mansijadiya1068-creator <mansijadiya1068@gmail.com>
2026-01-27 08:14:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,33 @@
+# calculate simple intrest
+principal=float(input("enter principal:"))
+r=float(input("enter rate of interest"))
+t=float(input("enter time (years):"))
+interest=(principal * rate * time)/100
+priont("simple interest is : {interest}")
+#find maximum of two numbers
+num1 = 10
+num2 = 25
+
+if num1 > num2:
+    maximum = num1
+else:
+    maximum = num2
+
+print(f"The maximum is {maximum}")
+#print nu,bers 1 to 5
+for i in range(1,6);
+print(I)
+S=input ("enter string")
+print("length=",len(s))
+print("welcome")
+s=input("enter string")
+print=("first charachter",s[0])
+n=int(input("enter number:"))
+if n > 0;
+     print("positive number")
+elif n < 0:
+    print ("negative number")
+else:
+    print("zero")
+x=input("enter something:")
+ print("you entered",x)
