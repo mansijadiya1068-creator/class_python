91d649ce521405f930b41142472d54ca61684712
mansijadiya1068-creator <mansijadiya1068@gmail.com>
2026-03-24 08:42:05 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,14 @@
+#def add (a,b):
+  #  print("a=",a)
+  #  print("b=",b)
+   # return a+b
+  #  result=add(2,5)
+  #  print("sum=",result)
+
+ def student_info(name,roll,marks):
+    print("name:",name)
+    print("roll no:",roll)
+    print("marks:",marks)
+    student_info("ravi",101,85)
+
+def
