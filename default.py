ae58fb08de04f78ef780bd2936743c2ea63c33f9
mansijadiya1068-creator <mansijadiya1068@gmail.com>
2026-03-24 08:30:23 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,29 @@
+def greet (name,msg="good morning"):
+    print("hello","name",",",msg)
+    greet("asha")
+    greet("ravi","good evening")
+
+    def power(num,exp=2):
+        return num ** exp
+        print(power(3))
+        print(power(3,3))
+        print(power(2,4))
+
+def simple_interest(P,r,n);
+si=(p*r*n)/100
+print("simple interest :",si)
+simple_interest(10000,2,2)
+simple_interest(50000,1.2,3)
+
+
+def ar_circle(r):
+    a_circle=3.14*r*r
+    print("area of circle :",a_circle)
+    ar__circle(1.5)
+    ar_circle(4)
+
+def check_value(no):
+    if(no>0):
+        print("positive")
+    else:
+     print("zero")
